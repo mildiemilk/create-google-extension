fa7cc8000b43295a1f7e4d9bfcd69019efa36351
mildiemilk <punthitra.dits@gmail.com>
2020-08-26 17:42:45 +0700

    Initial commit

---
+++ inject.js
@@ -0,0 +1,8 @@
+(function () {
+  var t3 = document.getElementById("lastname");
+  document.getElementById("name").value = "Johnny";
+  t3.value = "Bravo";
+  console.log(t3);//works;
+  return t3;
+
+})();
---
+++ popup.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('test');
+
+button.onclick = function (element) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(tabs[0].id, { file: 'inject.js' });
+  });
+};
